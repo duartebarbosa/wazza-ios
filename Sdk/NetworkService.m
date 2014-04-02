18cca64511d93f25a18dd7de9495f45e0c44aaad
Joao Vazão Vasques <joao.vasques@ist.utl.pt>
2014-04-02 16:19:35 +0100

    Minor refactoring

--- Sdk/NetworkService.m
+++ Sdk/NetworkService.m
@@ -9,52 +9,9 @@
 #import "NetworkService.h"
 #import "HttpCodes.h"
 
+#define URL @"http://localhost:9000/api/"
+
 @implementation NetworkService
-
--(NSString *)escapeURL:(NSString *)url {
-    NSString *newString = (__bridge NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (__bridge CFStringRef)url, NULL, CFSTR(" "), CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding));
-    
-    if (newString) {
-        return newString;
-    } else {
-        return @"";
-    }
-}
-
--(NSArray *)parseResponse: (NSData *)data :(NSError *)error {
-    return [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
-}
-
--(NSMutableURLRequest *)buildRequest:(NSString *)url
-                               :(NSString *)httpMethod
-                               :(NSDictionary *)params
-                               :(NSDictionary *)headers
-                               :(NSData *)data {
-
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[self escapeURL:url]]];
-    [request setHTTPMethod:httpMethod];
-
-    if ([httpMethod isEqualToString:HTTP_POST]) {
-        [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-        [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-        [request setValue:[NSString stringWithFormat:@"%d", [data length]] forHTTPHeaderField:@"Content-Length"];
-        [request setHTTPBody: data];
-    }
-
-    void(^addHeaders)(NSDictionary *) = ^(NSDictionary *h) {
-        if (h) {
-            NSMutableArray *keys = [[h allKeys] mutableCopy];
-            for (NSString *key in keys) {
-                [request setValue:[h objectForKey:key] forHTTPHeaderField:key];
-            }
-        }
-    };
-
-    addHeaders(params);
-    addHeaders(headers);
-
-    return request;
-}
 
 -(void)httpRequest:(int)reqType
                   :(NSString *)url
@@ -99,6 +56,53 @@
              }];
             break;
     }
+}
+
+#pragma mark Private Functions
+
+-(NSString *)escapeURL:(NSString *)url {
+    NSString *newString = (__bridge NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault, (__bridge CFStringRef)url, NULL, CFSTR(" "), CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding));
+    
+    if (newString) {
+        return newString;
+    } else {
+        return @"";
+    }
+}
+
+-(NSArray *)parseResponse: (NSData *)data :(NSError *)error {
+    return [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+}
+
+-(NSMutableURLRequest *)buildRequest:(NSString *)url
+                                    :(NSString *)httpMethod
+                                    :(NSDictionary *)params
+                                    :(NSDictionary *)headers
+                                    :(NSData *)data {
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[self escapeURL:url]]];
+    [request setHTTPMethod:httpMethod];
+    
+    if ([httpMethod isEqualToString:HTTP_POST]) {
+        [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+        [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+        [request setValue:[NSString stringWithFormat:@"%d", [data length]] forHTTPHeaderField:@"Content-Length"];
+        [request setHTTPBody: data];
+    }
+    
+    void(^addHeaders)(NSDictionary *) = ^(NSDictionary *h) {
+        if (h) {
+            NSMutableArray *keys = [[h allKeys] mutableCopy];
+            for (NSString *key in keys) {
+                [request setValue:[h objectForKey:key] forHTTPHeaderField:key];
+            }
+        }
+    };
+    
+    addHeaders(params);
+    addHeaders(headers);
+    
+    return request;
 }
 
 @end
