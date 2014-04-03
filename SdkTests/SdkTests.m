aa502fc693236cacd6b6ca6c3b8615e1a14996d8
Joao Vazão Vasques <joao.vasques@ist.utl.pt>
2014-04-03 17:32:13 +0100

    Updates test cases

--- SdkTests/SdkTests.m
+++ SdkTests/SdkTests.m
@@ -9,19 +9,36 @@
 #import <XCTest/XCTest.h>
 #import "Sdk.h"
 #import <CoreData/CoreData.h>
+#import "WazzaSDKDelegate.h"
 
-@interface SdkTests : XCTestCase
+@interface SdkTests : XCTestCase <WazzaSDKDelegate>
 
 @property(nonatomic, strong) SDK *sdk;
+@property(nonatomic, strong) dispatch_semaphore_t sem;
+@property(nonatomic) BOOL purchaseTestResult;
 
 @end
 
 @implementation SdkTests
 
+-(void)purchaseSuccess:(PurchaseInfo *)info {
+    self.purchaseTestResult = true;
+    dispatch_semaphore_signal(self.sem);
+    NSLog(@"SUCCESS!!!! %@", info);
+}
+
+-(void)PurchaseFailure:(NSError *)error {
+    self.purchaseTestResult = false;
+    dispatch_semaphore_signal(self.sem);
+    NSLog(@"FAIL!!!! %@", error);
+}
+
 - (void)setUp
 {
     [super setUp];
-    self.sdk = [[SDK alloc] initWithCredentials:@"App" :@"d7b7e2f5280e89236ed45474"];
+    self.sdk = [[SDK alloc] initWithCredentials:@"App" :@"d7b7e2f5280e89236ed45474" andLocation:false];
+    self.sdk.delegate = self;
+    self.sem = dispatch_semaphore_create(0);
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
@@ -39,6 +56,23 @@
 -(void)testTerminate {
     [self.sdk terminate];
     XCTAssertTrue(1==1);
+}
+
+-(void)testMakePurchase {
+    NSArray *items = [self.sdk getItems:1];
+    if (items == nil) {
+        XCTFail("list of items is null");
+    }
+    
+    Item *item = items[0];
+    if (item == nil) {
+        XCTFail("Item is null");
+    }
+    
+    [self.sdk makePurchase:item];
+    dispatch_semaphore_wait(self.sem, DISPATCH_TIME_FOREVER);
+    
+    XCTAssertTrue(self.purchaseTestResult);
 }
 
 @end
