cae3ceb39721821688ddae05bde8a0e740ee1b14
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2015-03-17 18:19:54 +0000

    Initial Commit

---
+++ Wazza-iOS-SDK/Wazza_iOS_SDK.h
@@ -0,0 +1,13 @@
+//
+//  Wazza_iOS_SDK.h
+//  Wazza-iOS-SDK
+//
+//  Created by Joao Luis Vazao Vasques on 17/03/15.
+//  Copyright (c) 2015 Wazza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Wazza_iOS_SDK : NSObject
+
+@end
---
+++ Wazza-iOS-SDK/Wazza_iOS_SDK.m
@@ -0,0 +1,13 @@
+//
+//  Wazza_iOS_SDK.m
+//  Wazza-iOS-SDK
+//
+//  Created by Joao Luis Vazao Vasques on 17/03/15.
+//  Copyright (c) 2015 Wazza. All rights reserved.
+//
+
+#import "Wazza_iOS_SDK.h"
+
+@implementation Wazza_iOS_SDK
+
+@end
